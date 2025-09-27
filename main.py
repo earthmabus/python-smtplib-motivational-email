09be4c00ee036a3df79048b67413a5b451652d57
Michael Popovich <earthmabus@hotmail.com>
2025-09-27 13:36:33 -0400

    sends out an inspirational quote when run

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import email_account
+import random
+import datetime as dt
 
 my_email = "earthmabus@gmail.com"
 
 PASSWORD_FILE = "./password.txt"
+QUOTES_FILE = "./quotes.txt"
 
 def load_password():
     '''loads the password for the email account from PASSWORD_FILE'''
@@ -11,18 +14,28 @@ def load_password():
         retval = file_password.readline().strip()
     return retval
 
+
+# load the inspirational quotes from the QUOTES_FILE
+def load_quotes():
+    retval = []
+    with open(QUOTES_FILE, "r") as file_quotes:
+        quotes = file_quotes.readlines()
+
+        for q in quotes:
+            items = q.split("-")
+            retval.append((items[0].strip(), items[1].strip()))
+    return retval
+
+# load the quotes and choose a random one to send
+quotes = load_quotes()
+chosen_quote = random.choice(quotes)
+
+# acquire the password for the account
 password = load_password()
+
+# acquire the date
+now = dt.datetime.now()
 
 # send an email
 account = email_account.EmailAccount(my_email="earthmabus@gmail.com", my_password=password)
-account.send_email("earthmabus@hotmail.com", "Testing from Python application", "testing my refactor")
-
-
-import datetime as dt
-
-now = dt.datetime.now()
-print(now)
-
-date_of_birth = dt.datetime(year=1978, month=1, day=2)
-print(date_of_birth)
-
+account.send_email("earthmabus@hotmail.com", f"Inspirational Quote of the Day ({now.year}/{now.month}/{now.day})", f"{chosen_quote[0]}\nby {chosen_quote[1]}")
